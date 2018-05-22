12e1c0dc3a7a58b682ee5148255a8dbbd7de1f3a
holddie <holddie@163.com>
2018-05-22 21:15:49 +0800

    :collision: eureka 服务

---
+++ holddie-center/src/main/java/com/holddie/cloud/holddiecenter/HolddieCenterApplication.java
@@ -0,0 +1,12 @@
+package com.holddie.cloud.holddiecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolddieCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolddieCenterApplication.class, args);
+	}
+}
